72be2cb5549883eae6942eb600f8ae2491f3c1fe
Alwin-Yang <alwinyang91@gmail.com>
2026-07-21 17:31:50 +0200

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,6 +4,11 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+-- Rendering: force the discrete NVIDIA GPU (this box has NVIDIA + AMD iGPU +
+-- llvmpipe; WebGpu can otherwise fall back to the iGPU or software rendering).
+config.front_end = "WebGpu"
+config.webgpu_power_preference = "HighPerformance"
+
 config.color_scheme = "Dracula (Official)"
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
