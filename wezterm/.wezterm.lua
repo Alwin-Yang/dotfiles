808756677a2c04da8ffe283a7619968d1f4285e7
Alwin-Yang <alwinyang91@gmail.com>
2026-07-21 19:18:52 +0200

    add keybindings for splitting panes in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,8 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
+local act = wezterm.action
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
@@ -18,6 +20,26 @@ config.font_size = 13
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.9
 -- config.macos_window_background_blur = 10
+
+config.keys = {
+    -- Ctrl+Shift++: new pane to the right
+    {
+      key = "mapped:+",
+      mods = "CTRL|SHIFT",
+      action = act.SplitPane {
+        direction = "Right",
+      },
+    },
+  
+    -- Ctrl+Shift+_: new pane below
+    {
+      key = "mapped:-",
+      mods = "CTRL|SHIFT",
+      action = act.SplitPane {
+        direction = "Down",
+      },
+    },
+  }
 
 -- and finally, return the configuration to wezterm
 return config
