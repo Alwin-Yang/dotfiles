2e551593ec284adcd978741c0cf0a51332f1df31
Alwin-Yang <alwinyang91@gmail.com>
2026-07-21 18:59:35 +0200

    add tmux

--- install.sh
+++ install.sh
@@ -69,6 +69,6 @@ brew install tmux
 echo "Stowing dotfiles..."
 cd "$HOME/dotfiles" || exit
 # Stow dotfiles packages
-stow -R -t ~ zsh wezterm p10k
+stow -R -t ~ zsh wezterm p10k tmux
 
 echo "Dotfiles setup complete!"
